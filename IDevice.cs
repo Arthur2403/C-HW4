432dba1f95723eb4e9f41e68e26eb913c3f14444
Arthur <arthurbej1@gmail.com>
2025-03-16 23:03:27 +0200

    Done Homework 4

---
+++ IDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace C_HW4
+{
+    internal interface IDevice
+    {
+        void Show();
+        void Sound();
+        void Desc();
+    }
+}
---
+++ IMusicalInstrument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_HW4
+{
+    internal interface IMusicalInstrument
+    {
+        string Name { get; set; }
+        void Sound();
+        void Show();
+        void History();
+        void Desc();
+    }
+}
